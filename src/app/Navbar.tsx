2189029341b2279a025f217b5e16c05beef6164d
Joshua Tenorio <joshliam09@gmail.com>
2023-10-31 01:25:10 -0700

    simplify simple navbar items

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -5,6 +5,18 @@ import { cn } from '~/lib/utils'
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from '~/ui/navigation-menu'
 import { Separator } from '~/ui/separator'
 
+function SimpleNavbarItem(props: any) {
+    return (
+        <NavigationMenuItem>
+        <Link href={props.href} legacyBehavior passHref>
+            <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "font-normal text-base my-1")}>
+                {props.desc}
+            </NavigationMenuLink>
+        </Link>
+        </NavigationMenuItem>
+    )
+}
+
 function Navbar() {
   return (
     <div>
@@ -17,27 +29,9 @@ function Navbar() {
                         </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link href="/files" legacyBehavior passHref>
-                        <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "font-normal text-base my-1")}>
-                            Project Files
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link href="/about" legacyBehavior passHref>
-                        <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "font-normal text-base my-1")}>
-                            About
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link href="/login" legacyBehavior passHref>
-                        <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "font-normal text-base my-1")}>
-                            Login
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
+                <SimpleNavbarItem href="/files" desc="Project Files"/>
+                <SimpleNavbarItem href="/about" desc="About"/>
+                <SimpleNavbarItem href="/login" desc="Login"/>
             </NavigationMenuList>
         </NavigationMenu>
         <Separator />
