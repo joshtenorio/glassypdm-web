525d4197413e0b4f241fcc3664a031b0dcc78ca4
Joshua Tenorio <joshliam09@gmail.com>
2024-01-01 15:48:17 -0700

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-
+import './index.css'
+import { Button } from '@/components/ui/button'
+import { ThemeProvider } from '@/components/theme-provider'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-    </>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <h1>Vite + React</h1>
+          <Button onClick={() => console.log("click!")}>
+            uWu
+          </Button>
+      </ThemeProvider>
   )
 }
 
