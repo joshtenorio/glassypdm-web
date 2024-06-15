206f1f876c04f241d780c27753b12b089c84e3ef
Joshua Tenorio <joshliam09@gmail.com>
2024-06-15 10:16:00 -0700

    add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './index.css'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -17,6 +18,8 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient()
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 // Render the app
 const rootElement = document.getElementById('root')!
@@ -25,7 +28,9 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      </QueryClientProvider>
       </ClerkProvider>
     </StrictMode>,
   )
