2c5d6e8cc7dbbf4fb21d06f5dce031d18ebd0e9b
Joshua Tenorio <joshliam09@gmail.com>
2024-06-15 10:13:31 -0700

    fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { SignedIn, SignedOut, SignIn } from '@clerk/clerk-react'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
